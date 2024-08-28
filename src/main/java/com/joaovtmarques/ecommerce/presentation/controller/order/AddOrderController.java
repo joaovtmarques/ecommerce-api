87c2027abd60583e8cc2430381eb810d36e3889b
João Vitor Marques <jvsilvam@outlook.com>
2024-08-28 09:40:57 -0300

    refactor: AddOrderController path

--- src/main/java/com/joaovtmarques/ecommerce/presentation/controller/order/AddOrderController.java
+++ src/main/java/com/joaovtmarques/ecommerce/presentation/controller/order/AddOrderController.java
@@ -1,4 +1,4 @@
-package com.joaovtmarques.ecommerce.presentation.order;
+package com.joaovtmarques.ecommerce.presentation.controller.order;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
